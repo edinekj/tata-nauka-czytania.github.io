7910b402b8b4dba6e93ee9db7fcac9e9aa684fb2
edinekj <edinekj@gmail.com>
2019-03-16 15:04:21 +0000

    Updates

---
+++ precache-manifest.4608cd6c55e7e9517cc8805d50fe0297.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0140496814b4a433c6a7",
+    "url": "/tata-nauka-czytania.github.io/static/css/main.6adf0233.chunk.css"
+  },
+  {
+    "revision": "0140496814b4a433c6a7",
+    "url": "/tata-nauka-czytania.github.io/static/js/main.700082ad.chunk.js"
+  },
+  {
+    "revision": "6b8047cde37a012e94e1",
+    "url": "/tata-nauka-czytania.github.io/static/js/runtime~main.845f5d14.js"
+  },
+  {
+    "revision": "f4fd9fa158c7ac215c2f",
+    "url": "/tata-nauka-czytania.github.io/static/js/2.8d1ae58a.chunk.js"
+  },
+  {
+    "revision": "bf02dfcc062afb988e74b69551be342c",
+    "url": "/tata-nauka-czytania.github.io/index.html"
+  }
+];
